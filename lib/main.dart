fba6a775a541cb4e2f6655bd6799705f488ba89d
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-01-29 13:43:23 +0300

    Maps & Routing; routes & initialRoute properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_world_clock_app/pages/home.dart';
+import 'package:my_world_clock_app/pages/loading.dart';
+import 'package:my_world_clock_app/pages/location.dart';
 
-void main() => runApp(const MaterialApp(
-  home: Home(),
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => const Loading(),
+    '/home': (context) => const Home(),
+    '/location': (context) => const Location(),
+  },
 ));
