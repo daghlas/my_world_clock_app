8ab2a2d08cfdf8d94ec6893046fe7e44237b420a
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-01-30 16:32:14 +0300

    flutter packages; http

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +9,19 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+
+    Response response = await get('https://jsonplaceholder.typicode.com/todos/1' as Uri);
+    print(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
