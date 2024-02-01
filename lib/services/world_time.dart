cac1eb75bf0c78721f2dd13fae54e262aefa552b
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-01 16:19:43 +0300

    custom class; world_time

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -8,4 +8,25 @@ class WorldTime{
   late String flag; // url to an asset flag
   late String url; //location url for API endpoint
 
+  void getTime() async {
+
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(dateTime);
+    // print(offset);
+
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set time property
+    time = now.toString();
+
+
+
+  }
+
 }
