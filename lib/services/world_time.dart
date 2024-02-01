67b5172cbce51c136e4578edbc2a3ef9cba4429a
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-01 16:07:02 +0300

    custom class; world_time

---
+++ lib/services/world_time.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime{
+
+  late String location; // location name for the UI
+  late String time; // time in that location
+  late String flag; // url to an asset flag
+  late String url; //location url for API endpoint
+
+}
