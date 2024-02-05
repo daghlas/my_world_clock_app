f1e82779458dcc82194c99c334450f8375f905a3
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-05 10:47:01 +0300

    home UI

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,12 +9,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   Map data = {};
 
   @override
   Widget build(BuildContext context) {
-
     data = ModalRoute.of(context)?.settings.arguments as Map;
     if (kDebugMode) {
       print(data);
@@ -22,16 +20,39 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          ElevatedButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, '/location');
-            },
-            icon: const Icon(Icons.edit_location),
-            label: const Text('Edit location'),
-          ),
-        ],
+          child: Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 120.0, 0.0, 0.0),
+        child: Column(
+          children: <Widget>[
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/location');
+              },
+              icon: const Icon(Icons.edit_location),
+              label: const Text('Edit location'),
+            ),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  data['location'],
+                  style: const TextStyle(
+                    fontSize: 28.0,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            Text(
+              data['time'],
+              style: const TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
