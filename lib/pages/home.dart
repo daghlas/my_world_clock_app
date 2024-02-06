ee68987ca59ffa4ba2765908712c4ea829a102d0
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-06 15:47:40 +0300

    updating time; final

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)?.settings.arguments as Map;
+    data = data.isNotEmpty ? data : ModalRoute.of(context)?.settings.arguments as Map;
     if (kDebugMode) {
       print(data);
     }
@@ -38,8 +38,16 @@ class _HomeState extends State<Home> {
           child: Column(
             children: <Widget>[
               ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
+                onPressed: () async {
+                  dynamic result = await Navigator.pushNamed(context, '/location');
+                  setState(() {
+                    data = {
+                      'time' : result['time'],
+                      'location' : result['location'],
+                      'isDaytime' : result['isDaytime'],
+                      'flag' : result['flag'],
+                    };
+                  });
                 },
                 icon: Icon(
                     Icons.edit_location,
