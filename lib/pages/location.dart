438ef07681b98ad8752e8cdfc98e247bd40bc687
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-06 10:04:24 +0300

    ListView builder; locations

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import '../services/world_time.dart';
 
 class Location extends StatefulWidget {
   const Location({super.key});
@@ -9,6 +10,17 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
   int counter = 0;
 
