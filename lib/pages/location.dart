e0f0b386d41b5dc3bcc51ada85fb6f36df16e285
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-06 15:13:55 +0300

    ListView builder; locations list

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../services/world_time.dart';
 
@@ -20,14 +19,19 @@ class _LocationState extends State<Location> {
     WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
     WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
     WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+    WorldTime(url: 'Europe/Budapest', location: 'Budapest', flag: 'hungary.png'),
+    WorldTime(url: 'Asia/Singapore', location: 'Singapore', flag: 'singapore.png'),
+    WorldTime(url: 'Africa/Johannesburg', location: 'Johannesburg', flag: 'south_africa.png'),
+    WorldTime(url: 'Europe/Vienna', location: 'Vienna', flag: 'austria.png'),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
         title: const Text('Choose Location'),
         centerTitle: true,
         elevation: 0,
@@ -39,6 +43,9 @@ class _LocationState extends State<Location> {
             child: ListTile(
               onTap: () {},
               title: Text(locations[index].location),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/${locations[index].flag}'),
+              ),
             ),
           );
           }
